06100e76d1933919595a8738593dd3b986ef0c9a
Pie <itspie@icloud.com>
2025-10-01 08:27:16 +0100

    Refactor IP to fruit mapping to word mapping

--- Utils/NetworkUtils.cs
+++ Utils/NetworkUtils.cs
@@ -8,50 +8,73 @@ namespace GameBox.Utils
 {
     public static class NetworkUtils
     {
-        // Mapping of IP last octets to fruit codes
-        private static readonly Dictionary<int, string> IpToFruit = new()
+        // Mapping of IP last octets (1-255) to unique, easy-to-type words
+        private static readonly string[] OctetWords = new string[]
         {
-            { 1, "Apple" },
-            { 2, "Blackberry" },
-            { 3, "Carrot" },
-            { 4, "Date" },
-            { 5, "Egg" },
-            { 6, "Fig" },
-            { 7, "Grape" },
-            { 8, "Honey" },
-            { 9, "Ice" },
-            { 10, "Jam" },
-            { 11, "Kiwi" },
-            { 12, "Lemon" },
-            { 13, "Mango" },
-            { 14, "Nut" },
-            { 15, "Orange" },
-            { 16, "Peach" },
-            { 17, "Quince" },
-            { 18, "Raspberry" },
-            { 19, "Strawberry" },
-            { 20, "Tomato" },
-            { 21, "Ugli" },
-            { 22, "Vanilla" },
-            { 23, "Watermelon" },
-            { 24, "Ximenia" },
-            { 25, "Yam" },
-            { 26, "Zucchini" }
+            // Fruits
+            "Apple", "Apricot", "Avocado", "Banana", "Berry", "Blackberry", "Blueberry", "Cherry", "Coconut", "Date",
+            "Fig", "Grape", "Guava", "Kiwi", "Lemon", "Lime", "Mango", "Melon", "Nectarine", "Orange",
+            "Papaya", "Peach", "Pear", "Pineapple", "Plum", "Raspberry", "Strawberry", "Tangerine", "Watermelon", "Olive",
+            // Vegetables
+            "Carrot", "Celery", "Corn", "Cucumber", "Eggplant", "Garlic", "Lettuce", "Onion", "Pea", "Pepper",
+            "Potato", "Pumpkin", "Radish", "Spinach", "Squash", "Tomato", "Turnip", "Yam", "Zucchini", "Bean",
+            // Nuts/Seeds
+            "Almond", "Cashew", "Hazelnut", "Peanut", "Pecan", "Pistachio", "Walnut", "Chia", "Flax", "Sesame",
+            // Animals (easy spelling)
+            "Cat", "Dog", "Duck", "Fox", "Goat", "Horse", "Lion", "Mouse", "Pig", "Rabbit",
+            "Sheep", "Tiger", "Wolf", "Bear", "Cow", "Deer", "Frog", "Hawk", "Mole", "Owl",
+            "Rat", "Seal", "Swan", "Whale", "Bat", "Bee", "Crab", "Crow", "Fish", "Goose",
+            "Lamb", "Mule", "Panda", "Shark", "Snail", "Snake", "Spider", "Squirrel", "Wasp", "Ant",
+            // Colors (easy)
+            "Red", "Blue", "Green", "Yellow", "Purple", "Pink", "Brown", "Black", "White", "Gray",
+            "Orange", "Gold", "Silver", "Tan", "Cyan", "Ivory", "Navy", "Teal", "Violet", "Magenta",
+            // Foods (easy, common)
+            "Bread", "Butter", "Cake", "Candy", "Cheese", "Chips", "Cookie", "Cream", "Egg", "Ham",
+            "Honey", "Jam", "Juice", "Milk", "Muffin", "Oat", "Oil", "Pie", "Rice", "Salt",
+            "Soup", "Sugar", "Toast", "Waffle", "Yogurt", "Bagel", "Bacon", "Donut", "Fruit", "Salad",
+            // Herbs/Spices (easy)
+            "Basil", "Chive", "Cilantro", "Dill", "Mint", "Parsley", "Rosemary", "Sage", "Thyme", "Peppercorn",
+            // More Fruits/Vegetables
+            "Acai", "Cantaloupe", "Jackfruit", "Kale", "Lime", "Mandarin", "Mulberry", "Passion", "Persimmon", "Quince",
+            "Starfruit", "Ugli", "Jicama", "Kumquat", "Lychee", "Rutabaga", "Shallot", "Sweetpea", "Endive", "Fennel",
+            // More Animals
+            "Hawk", "Jay", "Lynx", "Moose", "Orca", "Otter", "Robin", "Vole", "Vulture", "Yak",
+            // Short objects (easy to type)
+            "Ball", "Book", "Brush", "Cup", "Key", "Lamp", "Leaf", "Pen", "Rock", "Rope",
+            "Sand", "Sock", "Star", "Stick", "Stone", "Tape", "Tool", "Toy", "Twig", "Wheel",
+            // Even more foods/objects
+            "Beanie", "Bowl", "Fork", "Knife", "Plate", "Spoon", "Tray", "Drum", "Flag", "Glove",
+            "Hat", "Ring", "Sock", "Tile", "Tube", "Vase", "Zip", "Can", "Jar", "Pad",
+            // Short adjectives/colors
+            "Bright", "Calm", "Clear", "Cool", "Dim", "Dull", "Faint", "Grand", "Mild", "Rich",
+            "Sharp", "Shy", "Slick", "Slim", "Soft", "Solid", "Tame", "Wild", "Wise", "Young",
+            // More animals
+            "Goose", "Jay", "Lynx", "Moose", "Orca", "Otter", "Robin", "Vole", "Vulture", "Yak",
+            // More colors/objects
+            "Aqua", "Azure", "Berry", "Blush", "Cherry", "Coral", "Denim", "Flame", "Ivory", "Lemon",
+            "Mint", "Moss", "Peach", "Plum", "Ruby", "Sage", "Sky", "Snow", "Stone", "Sun",
+            // Extra simple foods/objects
+            "Ice", "Jam", "Nut", "Oat", "Oil", "Tea", "Vanilla", "Wheat", "Yolk", "Zest",
+            // Fill to 255 with repeat or easy randoms
+            "Fern", "Pine", "Lily", "Rose", "Tulip", "Daisy", "Cedar", "Palm", "Elm", "Oak",
+            "Beech", "Maple", "Willow", "Spruce", "Ash"
         };
 
-        private static readonly Dictionary<string, int> FruitToIp = new();
+        private static readonly Dictionary<int, string> IpToWord = new();
+        private static readonly Dictionary<string, int> WordToIp = new();
 
         static NetworkUtils()
         {
-            // Initialize reverse mapping
-            foreach (var kvp in IpToFruit)
+            // Initialize mapping
+            for (int i = 0; i < OctetWords.Length; i++)
             {
-                FruitToIp[kvp.Value.ToLower()] = kvp.Key;
+                IpToWord[i + 1] = OctetWords[i];
+                WordToIp[OctetWords[i].ToLower()] = i + 1;
             }
         }
 
         /// <summary>
-        /// Gets the local IP address of the machine
+        /// Gets the local IP address of the machine.
         /// </summary>
         public static string GetLocalIPAddress()
         {
@@ -74,22 +97,19 @@ namespace GameBox.Utils
         }
 
         /// <summary>
-        /// Converts an IP address to a fruit code
+        /// Converts an IP address to a word code.
         /// </summary>
-        public static string IpToFruitCode(string ipAddress)
+        public static string IpToWordCode(string ipAddress)
         {
             try
             {
                 var parts = ipAddress.Split('.');
                 if (parts.Length == 4 && int.TryParse(parts[3], out int lastOctet))
                 {
-                    // For IP addresses beyond our mapping, generate a deterministic fruit name
-                    if (lastOctet > 26)
-                    {
-                        var index = (lastOctet % 26) + 1;
-                        return IpToFruit.GetValueOrDefault(index, $"Fruit{lastOctet}");
-                    }
-                    return IpToFruit.GetValueOrDefault(lastOctet, $"Unknown{lastOctet}");
+                    if (lastOctet >= 1 && lastOctet <= OctetWords.Length)
+                        return IpToWord.GetValueOrDefault(lastOctet, $"Code{lastOctet}");
+                    else
+                        return $"Code{lastOctet}";
                 }
                 return "Unknown";
             }
@@ -100,15 +120,15 @@ namespace GameBox.Utils
         }
 
         /// <summary>
-        /// Converts a fruit code back to the last octet of an IP
+        /// Converts a word code back to the last octet of an IP.
         /// </summary>
-        public static int FruitCodeToLastOctet(string fruitCode)
+        public static int WordCodeToLastOctet(string wordCode)
         {
-            return FruitToIp.GetValueOrDefault(fruitCode.ToLower(), -1);
+            return WordToIp.GetValueOrDefault(wordCode.ToLower(), -1);
         }
 
         /// <summary>
-        /// Gets the network base (first three octets) of local IP
+        /// Gets the network base (first three octets) of local IP.
         /// </summary>
         public static string GetNetworkBase()
         {
@@ -122,19 +142,18 @@ namespace GameBox.Utils
         }
 
         /// <summary>
-        /// Constructs full IP from fruit code
+        /// Constructs full IP from word code.
         /// </summary>
-        public static string FruitCodeToIp(string fruitCode)
+        public static string WordCodeToIp(string wordCode)
         {
-            var lastOctet = FruitCodeToLastOctet(fruitCode);
+            var lastOctet = WordCodeToLastOctet(wordCode);
             if (lastOctet == -1) return string.Empty;
-            
             var networkBase = GetNetworkBase();
             return $"{networkBase}.{lastOctet}";
         }
 
         /// <summary>
-        /// Checks if an IP address is reachable
+        /// Checks if an IP address is reachable.
         /// </summary>
         public static bool IsIpReachable(string ipAddress, int timeoutMs = 1000)
         {
@@ -151,7 +170,7 @@ namespace GameBox.Utils
         }
 
         /// <summary>
-        /// Gets a random available port
+        /// Gets a random available port.
         /// </summary>
         public static int GetAvailablePort()
         {
